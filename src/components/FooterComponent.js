9f37a54e98704df83d05fc7b3022a9c83a1b70e6
Silvio Marques <silvio.mms2016@gmail.com>
2019-10-18 17:46:57 -0300

    Fixing DishDetailComponent

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -28,11 +28,9 @@ function Footer(props) {
                     </div>
                     <div className="col-12 col-sm-4 align-self-center">
                         <div className="text-center">
-                            <a className="btn btn-social-icon btn-google" href="http://google.com/+"><i className="fa fa-google-plus"></i></a>
                             <a className="btn btn-social-icon btn-facebook" href="http://facebook.com/"><i className="fa fa-facebook"></i></a>
                             <a className="btn btn-social-icon btn-linkedin" href="http://linkedin.com/"><i className="fa fa-linkedin"></i></a>
                             <a className="btn btn-social-icon btn-twitter" href="http://twitter.com/"><i className="fa fa-twitter"></i></a>
-                            <a className="btn btn-social-icon btn-google" href="http://youtube.com/"><i className="fa fa-youtube"></i></a>
                         </div>
                     </div>
                 </div>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,11 +4,13 @@ import Menu from './MenuComponent';
 import Contact from './ContactComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import DishDetail from './DishDetailComponent';
 import { DISHES } from '../storage/dishes';
 import { LEADERS } from '../storage/leaders';
 import { COMMENTS } from '../storage/comments';
 import { PROMOTIONS } from '../storage/promotions';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import { comment } from 'postcss';
 
 class Main extends Component {
 
@@ -33,12 +35,21 @@ class Main extends Component {
             );
         }
 
+        const DishWithId = ({match}) => {
+            return (
+                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+                    comments={this.state.comments.filter((comments) => comment.dishId) === parseInt(match.params.dishId, 10)}
+                />
+            ); 
+        }
+
         return (
             <div>
                 <Header />
                 <Switch>
                     <Route path="/home" component={HomePage} />
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route path="/menu/:dishId" component={DishWithId} />
                     <Route path="/contact" component={Contact} />
                     <Redirect to="/home" />
                 </Switch>
