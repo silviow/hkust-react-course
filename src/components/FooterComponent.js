334e69e969afa2f983259d99aa7865ab0097afbb
Silvio Marques <silvio@impyco.com.br>
2019-10-19 19:34:53 -0300

    Creating controlled form

---
+++ src/components/FooterComponent.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Footer(props) {
+    return (
+        <div className="footer">
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-4 offset-1 col-sm-2">
+                        <h5>Links</h5>
+                        <ul className="list-unstyled">
+                            <li><Link to="/home">Home</Link></li>
+                            <li><Link to="/about">About</Link></li>
+                            <li><Link to="/menu">Menu</Link></li>
+                            <li><Link to="/contact">Contact</Link></li>
+                        </ul>
+                    </div>
+                    <div className="col-7 col-sm-5">
+                        <h5>Our Address</h5>
+                        <address>
+                            121, Clear Water Bay Road<br />
+                            Clear Water Bay, Kowloon<br />
+                            Hong Kong<br />
+                            <i className="fa fa-phone fa-lg"></i>: +852 1234 5678<br />
+                            <i className="fa fa-envelope fa-lg"></i>: <a href="mailto:contact@restaurant.com">
+                                contact@restaurant.com</a>
+                        </address>
+                    </div>
+                    <div className="col-12 col-sm-4 align-self-center">
+                        <div className="text-center">
+                            <a className="btn btn-social-icon btn-facebook" href="http://facebook.com/"><i className="fa fa-facebook"></i></a>
+                            <a className="btn btn-social-icon btn-linkedin" href="http://linkedin.com/"><i className="fa fa-linkedin"></i></a>
+                            <a className="btn btn-social-icon btn-twitter" href="http://twitter.com/"><i className="fa fa-twitter"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div className="row justify-content-center">
+                    <div className="col-auto">
+                        <p>© Copyright 2019 Restaurant</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
---
+++ src/components/MainComponent.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import Home from './HomeComponent';
+import About from './AboutComponent';
+import Menu from './MenuComponent';
+import Contact from './ContactComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+import DishDetail from './DishDetailComponent';
+import { DISHES } from '../storage/dishes';
+import { LEADERS } from '../storage/leaders';
+import { COMMENTS } from '../storage/comments';
+import { PROMOTIONS } from '../storage/promotions';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+class Main extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            dishes: DISHES,
+            leaders: LEADERS,
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+        };
+    }
+
+    render() {
+        const HomePage = () => {
+            return (
+                <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                    leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                />
+            );
+        }
+
+        const DishWithId = ({match}) => {
+            return (
+                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+                    comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
+                />
+            ); 
+        }
+
+        return (
+            <div>
+                <Header />
+                <Switch>
+                    <Route path="/home" component={HomePage} />
+                    <Route path="/about" component={() => <About leaders={this.state.leaders} />} />
+                    <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route path="/menu/:dishId" component={DishWithId} />
+                    <Route path="/contact" component={Contact} />
+                    <Redirect to="/home" />
+                </Switch>
+                <Footer />
+            </div>
+        );
+    }
+}
+
+export default Main;
